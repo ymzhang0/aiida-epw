afcc068b9464263b03fa528788bc2c53c8c73200
ymzhang0 <zhangyiming1998@gmail.com>
2026-06-21 10:27:44 +0200

    Generalize mix_broyden factorization error pattern to match any info status code

--- src/aiida_epw/parsers/epw.py
+++ src/aiida_epw/parsers/epw.py
@@ -47,7 +47,7 @@ class EpwParser(BaseParser):
     class_error_map = {
         "Size of required memory exceeds max_memlt": "ERROR_MEMORY_EXCEEDS_MAX_MEMLT",
         "internal error, cannot bracket Ef": "ERROR_CANNOT_BRACKET_EF",
-        r"Error in routine mix_broyden \(5\):\s*factorization": "ERROR_FACTORIZATION",
+        r"Error in routine mix_broyden \(\d+\):\s*factorization": "ERROR_FACTORIZATION",
     }
 
     @staticmethod
