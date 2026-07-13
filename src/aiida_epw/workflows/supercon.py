68c697b53daead61fe7a0d32eedb893d3d6c9303
ymzhang0 <zhangyiming1998@gmail.com>
2026-07-13 13:01:50 +0200

    feat(supercon): exclude calculation_type and restart_type from exposed inputs, set them in run steps

--- src/aiida_epw/workflows/supercon.py
+++ src/aiida_epw/workflows/supercon.py
@@ -120,6 +120,8 @@ class SuperConWorkChain(ProtocolMixin, WorkChain):
                 "parent_folder_chk",
                 "qfpoints",
                 "kfpoints",
+                "calculation_type",
+                "restart_type",
             ),
             namespace_options={
                 "help": (
@@ -138,6 +140,8 @@ class SuperConWorkChain(ProtocolMixin, WorkChain):
                 "parent_folder_chk",
                 "qfpoints_distance",
                 "kfpoints_factor",
+                "calculation_type",
+                "restart_type",
             ),
             namespace_options={
                 "required": False,
@@ -158,6 +162,8 @@ class SuperConWorkChain(ProtocolMixin, WorkChain):
                 "parent_folder_chk",
                 "qfpoints_distance",
                 "kfpoints_factor",
+                "calculation_type",
+                "restart_type",
             ),
             namespace_options={
                 "required": False,
@@ -429,6 +435,16 @@ class SuperConWorkChain(ProtocolMixin, WorkChain):
         inputs.kfpoints_factor = self.inputs.epw_interp.kfpoints_factor
         inputs.qfpoints_distance = self.ctx.interpolation_list.pop()
 
+        base_inputs = EpwBaseWorkChain.spec().inputs
+        if "calculation_type" in base_inputs:
+            from aiida_epw.calculations.epw import serialize_calculation_type
+
+            inputs.calculation_type = serialize_calculation_type("eliashberg")
+        if "restart_type" in base_inputs:
+            from aiida_epw.calculations.epw import serialize_restart_type
+
+            inputs.restart_type = serialize_restart_type("ephwrite")
+
         if self.ctx.degaussq:
             parameters = inputs.parameters.get_dict()
             parameters["INPUTEPW"]["degaussq"] = self.ctx.degaussq
@@ -497,6 +513,16 @@ class SuperConWorkChain(ProtocolMixin, WorkChain):
         inputs.kfpoints = parent_folder_epw.creator.inputs.kfpoints
         inputs.qfpoints = parent_folder_epw.creator.inputs.qfpoints
 
+        base_inputs = EpwBaseWorkChain.spec().inputs
+        if "calculation_type" in base_inputs:
+            from aiida_epw.calculations.epw import serialize_calculation_type
+
+            inputs.calculation_type = serialize_calculation_type("eliashberg")
+        if "restart_type" in base_inputs:
+            from aiida_epw.calculations.epw import serialize_restart_type
+
+            inputs.restart_type = serialize_restart_type("ephread")
+
         if self.ctx.degaussq:
             parameters = inputs.parameters.get_dict()
             parameters["INPUTEPW"]["degaussq"] = self.ctx.degaussq
@@ -538,6 +564,16 @@ class SuperConWorkChain(ProtocolMixin, WorkChain):
         inputs.parent_folder_epw = parent_folder_epw
         inputs.kfpoints = parent_folder_epw.creator.inputs.kfpoints
         inputs.qfpoints = parent_folder_epw.creator.inputs.qfpoints
+
+        base_inputs = EpwBaseWorkChain.spec().inputs
+        if "calculation_type" in base_inputs:
+            from aiida_epw.calculations.epw import serialize_calculation_type
+
+            inputs.calculation_type = serialize_calculation_type("eliashberg")
+        if "restart_type" in base_inputs:
+            from aiida_epw.calculations.epw import serialize_restart_type
+
+            inputs.restart_type = serialize_restart_type("ephread")
 
         inputs.metadata.call_link_label = "epw_final_aniso"
         workchain_node = self.submit(EpwBaseWorkChain, **inputs)
